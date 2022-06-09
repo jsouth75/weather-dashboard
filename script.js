9ab5dbd3d2396c4bfa9e7b853410af1e080c9490
Jason South <jason.south@me.com>
2022-06-08 21:25:22 -0500

    set up structure & some design in css

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var city;
+var state;
+var country;
+
+var APIKey = "428fb4ba7c4828678c18c215cc7572b9";
+
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={APIkey}");
+
+
+
+
+
